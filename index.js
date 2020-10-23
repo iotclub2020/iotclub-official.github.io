41777d8178e12061f252ba651667c816b98d5829
Sankara <sankarasubramaniyan.ab@gmail.com>
2020-10-23 21:32:30 +0530

    Login added in index

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAMBlgm_BBWWVEIRmMc1ddSbfza9SdgILI",
+  authDomain: "iotclub-3b2cd.firebaseapp.com",
+  databaseURL: "https://iotclub-3b2cd.firebaseio.com",
+  projectId: "iotclub-3b2cd",
+  storageBucket: "iotclub-3b2cd.appspot.com",
+  messagingSenderId: "1031333943987",
+  appId: "1:1031333943987:web:43f55129846f1a754deec6",
+  measurementId: "G-82LG5CXE52"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
 function login(){
 
-  var userEmail = document.getElementById("email_field").value;
-  var userPass = document.getElementById("password_field").value;
+  var userEmail = document.getElementById("user_email").value;
+  var userPass = document.getElementById("user_password").value;
+  document.getElementById("btn-login").disabled = true;
 
-  var u = firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then(function (){
-    window.location = 'leaderboard.html';
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then(function (){
+    window.location = 'dashboard.html';
   }).catch(function(error) {
     var errorCode = error.code;
     var errorMessage = error.message;
-    window.alert("Error : " + errorMessage);
-
+    window.alert("The user details is incorrect !");
+    document.getElementById("btn-login").disabled = false;
   });
 
 }
 
 function logout(){
   firebase.auth().signOut();
+  window.location = 'index.html';
 }
