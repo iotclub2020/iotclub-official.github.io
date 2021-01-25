ff18ab01c4b3dd23e260e7e5bcc650c8a8888427
SankaraSubramaniyan <sankarasubramaniyan.ab@gmail.com>
2021-01-25 23:23:43 +0530

    Control for chat

--- teams/teamcontroller.js
+++ teams/teamcontroller.js
@@ -1,23 +1,44 @@
 var db = firebase.database().ref('/teams');
 window.localStorage.setItem("name","Team Sankar");
 window.localStorage.setItem("uid","8547");
+
 const teamName = window.localStorage.getItem("name");
 const uid = window.localStorage.getItem("uid");
+
+const post = document.getElementById('postMessage');
+var ctrlholder = document.getElementById('controlholder');
+
+var textarea = document.getElementById('messageArea');
+var parent = document.getElementById('messageList');
+
+function disableChat(){
+    if(uid != null){
+        post.style.visibility = "hidden";
+        ctrlholder.innerHTML = '<button class="btn btn-success pull-right" onclick="enableChat()">Enable Chat</button>';
+    }
+}
+
+function enableChat(){
+    if(uid != null){
+        post.style.visibility = "visible";
+        ctrlholder.innerHTML = '<button class="btn btn-danger pull-right" onclick="disableChat()">Stop Chat</button>';
+    }
+}
+
 function getMessage(){
-    var textarea = document.getElementById('messageArea');
     if (textarea.value != "")
         addMessage(textarea.value);
     textarea.value="";
 }
 
 function addMessage(replyText){
-    // var teamName = "Scriptons";
-    // var replyText = "Answer from team here";
+
     var list = '<li style="margin-bottom: 20px;" class="list-group-item">'+
     '<div class="card bg-light mb-3">'+
     '<div class="card-header h5"><b>'+
     teamName +
     '</b>';
+
     if (uid != null){
         list += '<button class="pull-right btn btn-danger"><div class="row">'+
         '<div class="col" style="margin-right: -25px"><i class="material-icons">add</i></div>'+
@@ -30,10 +51,20 @@ function addMessage(replyText){
       replyText +
       '</p></div></div></li>';
 
-    var parent = document.getElementById('messageList');
     parent.innerHTML += list;
-    // console.log(parent);
+    scrollDown();
+
     
 }
-console.log(window.localStorage.getItem("uid"));
-// addMessage();
+
+function scrollDown(){
+    var elem = document.getElementById('messageCard');
+    elem.scrollTop = elem.scrollHeight;
+}
+
+function keyEvent(event){
+    var x = event.key;
+    if (x == "Enter")
+        getMessage();
+    
+}
